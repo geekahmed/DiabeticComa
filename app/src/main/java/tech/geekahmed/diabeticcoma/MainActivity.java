2dac4aa5be5b166bf94dcce162a0799982e47420
Ahmed Moustafa <geekahmed1@gmail.com>
2021-05-01 22:52:28 +0200

    Project intialization

---
+++ app/src/main/java/tech/geekahmed/diabeticcoma/MainActivity.java
@@ -0,0 +1,14 @@
+package tech.geekahmed.diabeticcoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
